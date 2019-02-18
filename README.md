c4402260c65d8fc50604c0279bef883b012ff886
KOZ39 <koz39c@gmail.com>
2019-02-18 20:26:35 +0900

    Critical bug fix

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # BQM!
 > Be Quiet Mingosu!
 
+## 요구사항
+* [Python 3.6+](https://www.python.org/)
+* [acbpy (Destrapier)](https://github.com/Destrapier/acbpy)
+
 ## 사용법
 * 작업하려는 원본 *.acb.bytes 파일 백업
 * 드래그 앤드 드롭
--- src/bqm.py
+++ src/bqm.py
@@ -4,21 +4,22 @@ import sys
 import acbpy
 
 PRESET = b'\xff\xff' + b'\x00' * 166 + b'\xf9\xcf'
-HCA_LIST = []
 
 del sys.argv[0]
 
 if len(sys.argv) > 0:
     for argv in sys.argv:
         if os.path.isfile(argv):
+            hcaList = []
+
             with open(argv, 'rb') as f:
                 for i in acbpy.parse_binary(f):
-                    HCA_LIST.append(i.binary.read())
+                    hcaList.append(i.binary.read())
 
             with open(argv, 'rb+') as f:
                 data = f.read()
 
-                for hca in HCA_LIST:
+                for hca in hcaList:
                     data = data.replace(hca, hca[:96] + PRESET * int(len(hca) / len(PRESET)))
 
                 f.seek()
