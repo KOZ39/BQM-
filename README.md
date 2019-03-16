e0aa4b44210a7cda6c8cd79c870b614815f744df
KOZ39 <koz39c@gmail.com>
2019-03-16 19:02:22 +0900

    Pre-Release

--- README.md
+++ README.md
@@ -1,24 +1,53 @@
 # BQM!
 > Be Quiet Mingosu!
 
-## 요구사항
-### 공통
+## 데모 영상
+[![웰로드 보이스 제거](https://img.youtube.com/vi/5yHY8cjzmKw/0.jpg)](https://www.youtube.com/watch?v=5yHY8cjzmKw)
+
+## 요구사항(*.py 사용시, *.exe는 미해당)
+**공통**
 * [Python 3.6+](https://www.python.org/)
 * [acbpy (CrescentApricot)](https://github.com/CrescentApricot/acbpy)
-### w2b.py
+
+**w2b.py**
 * [SonicAudioTools (blueskythlikesclouds)](https://github.com/blueskythlikesclouds/SonicAudioTools)
 
 ## 사용법
-### bqm.py
+**bqm**
+> 작업하는 *.acb.bytes를 덮어씌우므로 백업을 권장합니다.
 ```
-* 작업하려는 원본 *.acb.bytes 파일 백업
-* 드래그 앤드 드롭
+* bqm.py 또는 bqm.exe에 *.acb.bytes 드래그 앤드 드롭
 ```
-### w2b.py
+**w2b**
+> Welrod (1).acb.bytes이 결과물이므로, 덮어씌울때는 Welrod.acb.bytes로 파일명 변경이 필요합니다.
 ```
-* BrenMK.acb.bytes, Welrod.acb.bytes, SonicAudioTools 준비
-* 더블 클릭
+* BrenMK.acb.bytes, Welrod.acb.bytes, SonicAudioTools(w2b.py 사용시에만 해당) 준비
+* w2b.py 또는 w2b.exe 더블 클릭
 ```
+
+원하시는 작업 후 알맞은 경로에 덮어씌워주시면 됩니다.
+
+## 서버 별 경로
+> 클라이언트 버전 2.0300부터는 /New 폴더가 존재하니 해당 폴더에 덮어씌워주시면 됩니다.
+* 운영사
+	* 내부 저장소/Android/data/com.digitalsky.girlsfrontline.cn/files/Android/New
+* 비리비리
+	* 내부 저장소/Android/data/com.digitalsky.girlsfrontline.cn.bili/files/Android/New
+* 대만
+	* 내부 저장소/Android/data/tw.txwy.and.snqx/files/Android/New
+* 한국
+	* 내부 저장소/Android/data/kr.txwy.and.snqx/files/Android/New
+* 글로벌
+	* 내부 저장소/Android/data/com.sunborn.girlsfrontline.en/files/Android
+* 일본
+	* 내부저장소/Android/data/com.sunborn.girlsfrontline.jp/files/Android
+
+## Archive?
+기본 branch 변경으로 PR을 방지하기 위해 했습니다.  
+건의, 문의, 버그 제보는 아래 연락처로 부탁드립니다.
+* Email: [koz39c@gmail.com](mailto:koz39c@gmail.com)
+* Discord: KOZ39#2590
+* Telegram: [KOZ39](https://t.me/koz39)
 
 ## License
 [MIT](https://github.com/KOZ39/BQM-/blob/master/LICENSE)
--- src/w2b.py
+++ src/w2b.py
@@ -1,6 +1,7 @@
 import os
 import subprocess
 import shutil
+import sys
 
 import acbpy
 
@@ -9,24 +10,26 @@ PRESET = b'\xff\xff' + b'\x00' * 166 + b'\xf9\xcf'
 WELROD_SIZE = os.path.getsize('Welrod.acb.bytes')
 BRENMK_SIZE = os.path.getsize('BrenMK.acb.bytes')
 
-shutil.copy2('Welrod.acb.bytes', 'SonicAudioTools/Welrod.acb')
+MEIPASS = getattr(sys, '_MEIPASS') if hasattr(sys, '_MEIPASS') else '.'
 
-subprocess.call(['SonicAudioTools/AcbEditor.exe', 'SonicAudioTools/Welrod.acb'])
+shutil.copy2('Welrod.acb.bytes', f'{MEIPASS}/SonicAudioTools/Welrod.acb')
+
+subprocess.call([f'{MEIPASS}/SonicAudioTools/AcbEditor.exe', f'{MEIPASS}/SonicAudioTools/Welrod.acb'])
 
 with open('BrenMK.acb.bytes', 'rb') as f:
     for i in acbpy.parse_binary(f):
-        if i.track.cue_id < 14:   
-            with open(f'SonicAudioTools/Welrod/{str(i.track.cue_id).zfill(5)}.{i.extension}', 'wb') as s:
+        if i.track.cue_id < 14:
+            with open(f'{MEIPASS}/SonicAudioTools/Welrod/{str(i.track.cue_id).zfill(5)}.{i.extension}', 'wb') as s:
                 s.write(i.binary.read())
         else:
-            with open(f'SonicAudioTools/Welrod/{str(i.track.cue_id+1).zfill(5)}.{i.extension}', 'wb') as s:
+            with open(f'{MEIPASS}/SonicAudioTools/Welrod/{str(i.track.cue_id+1).zfill(5)}.{i.extension}', 'wb') as s:
                 s.write(i.binary.read())
 
-with open('SonicAudioTools/Welrod/00014.hca', 'rb+') as f:
+with open(f'{MEIPASS}/SonicAudioTools/Welrod/00014.hca', 'rb+') as f:
     data = f.read()
     f.seek(0)
     f.write(data[:96] + (PRESET * int((WELROD_SIZE - BRENMK_SIZE) / len(PRESET)))[:-256])
 
-subprocess.call(['SonicAudioTools/AcbEditor.exe', 'SonicAudioTools/Welrod'])
+subprocess.call([f'{MEIPASS}/SonicAudioTools/AcbEditor.exe', f'{MEIPASS}/SonicAudioTools/Welrod'])
 
-shutil.copy2('SonicAudioTools/Welrod.acb', 'SonicAudioTools/Welrod.acb.bytes')
+shutil.copy2(f'{MEIPASS}/SonicAudioTools/Welrod.acb', f'{os.getcwd()}/Welrod (1).acb.bytes')
